62793f24fa61da471913de06c9b0b127599d90f4
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-16 15:41:33 -0300

    Adicionado imagens dos ambientes e configurado o popup

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,17 @@ menuMobile.addEventListener('click', ()=>{
 overlayMobile.addEventListener('click', ()=>{
     menuMobile.classList.remove('openMenu')
 })
+
+
+// popup das imagens
+
+function abrirPopup(ambiente) {
+    var imagemSrc = './src/img/' + ambiente + '.JPEG'; // Constrói o caminho da imagem
+    document.getElementById('imagemGrande').src = imagemSrc; // Define o 'src' para a imagem do popup
+    document.getElementById('popupImagem').style.display = 'flex'; // Mostra o popup
+  }
+
+  // Quando o botão de fechar é clicado
+  document.getElementById('fecharPopup').onclick = function() {
+    document.getElementById('popupImagem').style.display = 'none'; // Oculta o popup
+  };
